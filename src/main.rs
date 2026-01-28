b30f94399e881cb880226def9ccaf4bfb561d9b3
Iqbal <aodihis@gmail.com>
2026-01-28 12:45:17 +0700

    Add main

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use macroquad::prelude::*;
+use crate::constant::{MAP_COLS, MAP_ROWS, T_SIZE};
+use crate::game::Game;
+
+mod constant;
+mod game;
+mod helpers;
+
+enum GameState {
+    Menu,
+    Playing,
+    GameOver,
+}
+#[macroquad::main("Cat")]
+async fn main() {
+    let mut game = Game::new();
+    loop {
+        // set_camera(&game.cam);
+        clear_background(WHITE);
+
+        match game.state {
+            GameState::Menu => {
+                draw_text("Press enter to start!", 100., 100., 42., BLACK);
+                if is_key_pressed(KeyCode::Enter) {
+                    game.state = GameState::Playing;
+                }
+            },
+            GameState::Playing => {
+                game.draw();
+            },
+            GameState::GameOver => {},
+        }
+        next_frame().await;
+    }
+}
